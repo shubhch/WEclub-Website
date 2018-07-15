20f3603df53039ebc1d57321a80e5fe53add4f17
Shubham Chaurasiya <30908556+shubhch@users.noreply.github.com>
2018-07-16 00:43:08 +0530

    Add files via upload

---
+++ home/footer1.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+    <h4 align="right" style=font-size: 12px>
+       Website Designed & created By :<br/>
+                   Shivam Tripathi & WeClub developer's Team   </h4>
+    </body>
+</html>
